a5dd73d0dcc31fc2f7a2b9d7e04ff407a05d24ed
dizyd <David.Izydorczyk@gmail.com>
2024-07-05 13:00:09 +0200

    webpage

--- Data/download_data_from_OSF.R
+++ Data/download_data_from_OSF.R
@@ -1,5 +1,13 @@
 # Downloads the data file from the OSF into the corresponding folder
-# requires install.packages("osfr)
+# requires install.packages("osfr")
 
-osfr::osf_download(osf_retrieve_file("9tn4e"),path = "Data")
- 
+library(osfr)
+
+
+
+# Download all .csv files from the Data component 
+osf_download(osf_ls_files(osf_retrieve_node("y2rn8")),path = "Results")
+
+# Download a specific file
+osf_download(osf_retrieve_file("gva9p"),path = "Data") 
+osf_download(osf_retrieve_file("7htym"),path = "Data") # df_analysis.csv (cleaned and filtered df)
